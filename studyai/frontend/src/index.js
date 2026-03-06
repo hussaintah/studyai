10888c521ae5528c4d889ffe6c316598f10e49de
hussaintah <87081073+hussaintah@users.noreply.github.com>
2026-03-06 23:33:33 +0530

    Update index.js

--- studyai/frontend/src/index.js
+++ studyai/frontend/src/index.js
@@ -1,22 +1,10 @@
-<!DOCTYPE html>
-<html lang="en" data-theme="dark">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#0d0b08" />
-    <title>StudyAI</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=DM+Mono:wght@400;500&family=Instrument+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
-    <script>
-      // Restore saved theme before React loads to avoid flash
-      (function() {
-        var t = localStorage.getItem('studyai_theme');
-        if (t) document.documentElement.setAttribute('data-theme', t);
-      })();
-    </script>
-  </head>
-  <body>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root"></div>
-  </body>
-</html>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
